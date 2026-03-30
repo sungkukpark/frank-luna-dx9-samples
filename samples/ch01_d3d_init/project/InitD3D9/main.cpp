3e64feb8aacca8c6f576456fcbfb376da4453465
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-03-30 18:07:04 +0200

    Update project files

---
+++ samples/ch01_d3d_init/project/InitD3D9/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>	// Include the standard input/output header
+
+int main(void)		// Out program starts here
+{
+	printf("hello, world!");	// print "hello, world!" into the console
+	return;						// return zero to windows
+}
